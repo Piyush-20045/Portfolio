e2085c87e1d618b9fc0870127f133d58bedc5890
Piyush Yadav <piyushyadav20045@gmail.com>
2026-01-07 21:48:25 +0530

    ui: rewamp the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { LuSun, LuMoon } from "react-icons/lu";
 
@@ -16,61 +16,25 @@ const NavLink = ({ href, children }) => (
 // Main Navbar Component
 const Navbar = ({ darkMode, setDarkMode }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [show, setShow] = useState(true);
-
-  // scroll detect logic
-  useEffect(() => {
-    let lastY = window.scrollY;
-
-    const handleScroll = () => {
-      const currentY = window.scrollY;
-      if (currentY > lastY) setShow(false); // scrolling down
-      else setShow(true); // scrolling up
-      lastY = currentY;
-
-      // auto-close mobile menu on scroll
-      if (isOpen) setIsOpen(false);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [isOpen]);
 
   const navLinks = [
     { href: "#home", text: "Home" },
     { href: "#skills", text: "Skills" },
     { href: "#projects", text: "Projects" },
-    { href: "#contact", text: "Contact" },
   ];
 
   return (
-    <nav
-      className={`fixed top-0 left-0 right-0 z-50 transition-transform duration-300 ${
-        show ? "translate-y-0" : "-translate-y-full"
-      }`}
-    >
-      <div className="mt-2 md:w-fit mx-6 md:mx-auto px-6 py-3 border dark:border-b-white rounded-full dark:shadow-lg dark:shadow-gray-900 shadow-xl backdrop-blur-md">
+    <nav className="fixed top-0 left-0 right-0 z-50 transition-transform duration-300 max-w-4xl mx-auto">
+      <div className="mt-2 mx-3 md:mx-6 px-6 py-2 border dark:border-b-white rounded-md dark:shadow-lg dark:shadow-gray-900 shadow-xl backdrop-blur-md">
         <div className="flex justify-between items-center">
           {/* Logo */}
-          <motion.a
-            className="flex items-center gap-1 text-2xl text-gray-800 dark:text-white font-bold font-logo"
-            href="#home"
-            whileHover="hover"
-            rotate="rest"
-          >
+          <a href="#home">
             <motion.img
-              className="h-9 w-9 mt-1 rounded-full"
-              src="/favicon.ico"
-              variants={{
-                rest: { rotate: 0 },
-                hover: {
-                  rotate: 360,
-                  transition: { repeat: Infinity, duration: 1, ease: "linear" },
-                },
-              }}
+              className="h-11 w-11 bg-blue-300 rounded-md transition-all duration-300 ease-in-out hover:scale-90 dark:bg-yellow-300"
+              src="/hero.png"
+              whileHover="hover"
             />
-            PIYUSH
-          </motion.a>
+          </a>
 
           {/* Desktop Navigation */}
           <div className="hidden pl-20 text-gray-800 dark:text-white md:flex justify-center items-center space-x-7 lg:space-x-12">
@@ -83,54 +47,60 @@ const Navbar = ({ darkMode, setDarkMode }) => {
             <a
               href="https://drive.google.com/file/d/1gC2wTk0MHAVnK8YRHs_2ANIoEi_2PmPo/view?usp=drive_link"
               target="_blank"
-              className="px-3 py-1.5 text-white bg-gray-800 transition-all duration-300 rounded shadow-md hover:bg-gray-700 shadow-black dark:shadow dark:shadow-amber-100 active:shadow-none active:scale-95"
+              className="px-3 py-1.5 font-base text-black dark:text-white rounded hover:bg-gray-100 dark:hover:bg-gray-950 shadow shadow-black dark:shadow-white transition-all duration-300 active:shadow-none active:scale-95"
             >
               Resume
             </a>
           </div>
 
-          {/* DARK MODE btn */}
-          <button
-            onClick={() => {
-              setDarkMode(!darkMode);
-            }}
-            className={`hidden md:flex md:ml-20 w-14 px-1 py-1 rounded-full cursor-pointer shadow shadow-black dark:shadow-white transition-colors duration-300 ease-in-out ${
-              darkMode ? "bg-indigo-600" : "bg-sky-300"
-            }`}
-          >
-            <span
-              className={`p-1 bg-white rounded-full transition-transform duration-300 ${
-                darkMode
-                  ? "translate-x-6 text-yellow-600"
-                  : "translate-x-0 text-blue-700"
-              }`}
-            >
-              {darkMode ? <LuSun /> : <LuMoon />}
-            </span>
-          </button>
-
-          {/* Mobile Menu Button */}
-          <div className="md:hidden">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="focus:outline-none text-gray-800 dark:text-white"
-            >
-              <svg
-                className="w-8 h-8"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+          <div className="flex gap-4">
+            {/* Mobile Menu Button */}
+            <div className="mt-1 md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="focus:outline-none text-gray-800 dark:text-white"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={
-                    !isOpen ? "M4 6h16M4 12h16m-7 6h7" : "M6 18L18 6M6 6l12 12"
-                  }
-                ></path>
-              </svg>
+                <svg
+                  className="w-8 h-8"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d={
+                      !isOpen
+                        ? "M4 6h16M4 12h16m-7 6h7"
+                        : "M6 18L18 6M6 6l12 12"
+                    }
+                  ></path>
+                </svg>
+              </button>
+            </div>
+
+            {/* DARK MODE btn */}
+            <button
+              onClick={() => {
+                setDarkMode(!darkMode);
+              }}
+              className={`flex justify-center items-center md:ml-20 w-9 h-9 px-1 py-1 rounded-md cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-900 border border-neutral-200/60 dark:border-neutral-800 shadow shadow-black dark:shadow-white transition-all duration-300 ease-in-out active:scale-95`}
+            >
+              <span
+                className={`p-1 transition-transform duration-300 ${
+                  darkMode
+                    ? "rotate-0 text-gray-300"
+                    : "rotate-90 text-gray-800"
+                }`}
+              >
+                {darkMode ? (
+                  <LuMoon size={18} strokeWidth={2} />
+                ) : (
+                  <LuSun size={18} strokeWidth={2} />
+                )}
+              </span>
             </button>
           </div>
         </div>
@@ -138,7 +108,7 @@ const Navbar = ({ darkMode, setDarkMode }) => {
 
       {/* Mobile Menu */}
       <div
-        className={`md:hidden transition-all duration-300 ease-in-out overflow-hidden ${
+        className={`mt-1 mx-3 rounded-md md:hidden transition-all duration-300 ease-in-out overflow-hidden ${
           isOpen ? "max-h-screen" : "max-h-0"
         }`}
       >
@@ -161,24 +131,6 @@ const Navbar = ({ darkMode, setDarkMode }) => {
           >
             Resume
           </a>
-          <button
-            onClick={() => {
-              setDarkMode(!darkMode);
-            }}
-            className={`w-14 px-1 py-1 rounded-full cursor-pointer shadow shadow-black dark:shadow-white transition-colors duration-300 ease-in-out flex ${
-              darkMode ? "bg-indigo-600" : "bg-sky-300"
-            }`}
-          >
-            <span
-              className={`p-1 bg-white rounded-full transition-transform duration-400 ${
-                darkMode
-                  ? "translate-x-6 text-yellow-600"
-                  : "translate-x-0 text-blue-700"
-              }`}
-            >
-              {darkMode ? <LuSun /> : <LuMoon />}
-            </span>
-          </button>
         </div>
       </div>
     </nav>
